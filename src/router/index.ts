8a4c5c1a0bff87d99bab227508f1f1e80c066d99
Eduardo Brito <developer.edurbrito@gmail.com>
2025-02-13 21:45:44 +0200

    add home gif

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import FeaturesView from '@/views/FeaturesView.vue'
 import UniverseView from '@/views/UniverseView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -27,6 +27,11 @@ const router = createRouter({
     //   name: 'universe',
     //   component: UniverseView,
     // },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: HomeView,
+    },
   ],
 })
 
--- src/views/FeaturesView.vue
+++ src/views/FeaturesView.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>This is a features page</h1>
+    <!-- <h1>This is a features page</h1> -->
   </div>
 </template>
