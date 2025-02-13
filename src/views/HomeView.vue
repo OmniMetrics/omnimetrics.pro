9edc310933ad89388153c35f26f4471c76cc0c1f
Eduardo Brito <developer.edurbrito@gmail.com>
2025-02-13 23:12:43 +0200

    add features

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -237,31 +237,5 @@ import Logo from '@/components/Logo.vue'
         JOIN THE OM PLATFORM NOW!
       </button>
     </div> -->
-    <section class="relative flex items-center justify-center mt-20">
-      <div class="absolute w-full h-0.5 bg-white"></div>
-      <div
-        class="w-32 h-32 border-2 border-white rounded-full bg-transparent"
-      ></div>
-    </section>
-    <section class="flex flex-col items-center justify-center mt-20 relative">
-      <h2 class="text-4xl font-bold text-secondary uppercase text-center z-30">
-        READY TO ELEVATE YOUR PERFORMANCE?
-      </h2>
-      <button
-        class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] font-bold uppercase rounded-full p-0.5 mt-8 z-30"
-      >
-        <span
-          class="flex py-4 px-24 w-full text-sm text-black bg-white rounded-full p-2"
-          @click="$emit('open-modal')"
-        >
-          DROP US A MESSAGE
-        </span>
-      </button>
-      <img
-        src="@/assets/img/home-contact.png"
-        alt="Elevate Performance"
-        class="hidden md:block absolute left-0 transform -translate-x-1/3 mt-24 pb-4 w-2/4 z-20"
-      />
-    </section>
   </main>
 </template>
