8eb1e648aab9a9cf9cc17f3823c94452c5553215
Eduardo Brito <developer.edurbrito@gmail.com>
2025-02-13 23:12:43 +0200

    add features

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -47,7 +47,7 @@ import Logo from '@/components/Logo.vue'
         <p
           class="max-w-3xl mx-auto text-center text-sm md:text-base text-gray-300 leading-relaxed uppercase"
         >
-          Omnimetrics is founded on the synergy of two professionals whose
+          Omnimetrics is founded on the synergy of multiple professionals whose
           complementary expertise drives the company's vision and operations.
         </p>
       </section>
@@ -109,7 +109,7 @@ import Logo from '@/components/Logo.vue'
       </section>
 
       <section
-        class="flex flex-col md:flex-row mx-auto items-start w-full max-w-5xl py-8 text-gray-100"
+        class="flex flex-col-reverse md:flex-row mx-auto items-start w-full max-w-5xl py-8 text-gray-100"
       >
         <!-- LEFT SIDE (Image) -->
         <div
@@ -265,34 +265,6 @@ import Logo from '@/components/Logo.vue'
             aria-hidden="true"
           ></span>
         </div>
-      </section>
-      <section class="relative flex items-center justify-center mt-20">
-        <div class="absolute w-full h-0.5 bg-white"></div>
-        <div
-          class="w-32 h-32 border-2 border-white rounded-full bg-transparent"
-        ></div>
-      </section>
-      <section class="flex flex-col items-center justify-center mt-20 relative">
-        <h2
-          class="text-4xl font-bold text-secondary uppercase text-center z-30"
-        >
-          READY TO ELEVATE YOUR PERFORMANCE?
-        </h2>
-        <button
-          class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] font-bold uppercase rounded-full p-0.5 mt-8 z-30"
-        >
-          <span
-            class="flex py-4 px-24 w-full text-sm text-black bg-white rounded-full p-2"
-            @click="$emit('open-modal')"
-          >
-            DROP US A MESSAGE
-          </span>
-        </button>
-        <img
-          src="@/assets/img/home-contact.png"
-          alt="Elevate Performance"
-          class="hidden md:block absolute left-0 transform -translate-x-1/3 mt-24 pb-4 w-2/4 z-20"
-        />
       </section>
     </div>
   </div>
--- src/views/FeaturesView.vue
+++ src/views/FeaturesView.vue
@@ -1,5 +1,182 @@
+<script setup lang="ts">
+import Logo from '@/components/Logo.vue'
+</script>
+
 <template>
   <div>
-    <!-- <h1>This is a features page</h1> -->
+    <section class="w-full text-gray-100 py-8 px-4 bg-transparent">
+      <!-- Logo / Title Row at the Top -->
+      <div class="flex justify-center items-center mb-4">
+        <Logo class="hidden md:block" />
+      </div>
+
+      <!-- Main Heading -->
+      <h1
+        class="text-center text-3xl md:text-4xl font-bold text-secondary mb-2 uppercase"
+      >
+        UNLOCK YOUR TEAM'S FULL POTENTIAL
+      </h1>
+
+      <!-- Subheading / Tagline -->
+      <p
+        class="max-w-3xl mx-auto text-center text-sm md:text-base text-gray-300 leading-relaxed uppercase"
+      >
+        Our platform provides advanced, AI-driven analysis that helps coaches
+        and players understand the game from every angle Whether it's
+        in-possession, out-of-possession, or player fitness metrics.
+      </p>
+    </section>
+
+    <section
+      class="bg-cover bg-center bg-repeat-space"
+      style="
+        background-image: url('home-section-bg.png');
+        background-blend-mode: saturation;
+        background-size: 70%;
+      "
+    >
+      <div class="p-8 rounded-lg flex flex-col md:flex-row items-center gap-8">
+        <div
+          class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] p-1 rounded-3xl w-full md:w-2/3 shadow-lg"
+        >
+          <div class="bg-white p-6 rounded-3xl">
+            <h2 class="text-xl font-bold text-accent">
+              GAIN THE UPPER HAND WHEN YOUR TEAM HAS THE BALL
+            </h2>
+            <p class="mt-4 text-black">
+              Track how long and effectively your team maintains control.
+              Measure passing accuracy, key passes, and distribution. Analyze
+              success rates and distances covered during dribbles. Evaluate shot
+              selection, accuracy, and goal-scoring chances. Understand the
+              effectiveness of your offensive shape and player positioning.
+              Examine the precision and strategy behind your corners,
+              free-kicks, and offensive dead-ball situations.
+            </p>
+            <p class="mt-4 text-accent font-bold">AND MANY MORE!</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 flex flex-col gap-2 text-left">
+          <h3 class="text-2xl font-semibold text-white uppercase">
+            IN-POSSESSION METRICS
+          </h3>
+          <div class="border-t border-gray-500 w-full mx-auto mb-4"></div>
+          <p class="text-red-500 font-bold text-xl">POSSESSION</p>
+          <p class="text-red-500 font-bold text-xl">PASSING</p>
+          <p class="text-red-500 font-bold text-xl">DRIBBLING</p>
+          <p class="text-red-500 font-bold text-xl">SHOOTING</p>
+          <p class="text-red-500 font-bold text-xl">OFFENSIVE MOVEMENT</p>
+          <p class="text-red-500 font-bold text-xl">SET-PIECES</p>
+        </div>
+      </div>
+      <div class="p-8 rounded-lg flex flex-col md:flex-row items-center gap-8">
+        <div
+          class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] p-1 rounded-3xl w-full md:w-2/3 shadow-lg"
+        >
+          <div class="bg-white p-6 rounded-3xl">
+            <h2 class="text-xl font-bold text-accent">
+              DICTATE THE GAME THROUGH DEFENSIVE PRECISION
+            </h2>
+            <p class="mt-4 text-black">
+              See how well your team presses and forces turnovers. Monitor
+              defensive actions and successful ball recoveries. Track your
+              players' one-on-one challenges and successes. Assess your team's
+              defensive shape, especially during opposition attacks. Measure how
+              well your team prevents shot creation. Get insights on your
+              defensive strategy during free kicks, corners, and other set-piece
+              plays.
+            </p>
+            <p class="mt-4 text-accent font-bold">AND MANY MORE!</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 flex flex-col gap-2 text-left">
+          <h3 class="text-2xl font-semibold text-white uppercase">
+            OUT-OF-POSSESSION METRICS
+          </h3>
+          <div class="border-t border-gray-500 w-full mx-auto mb-4"></div>
+          <p class="text-blue-500 font-bold text-xl">PRESSING</p>
+          <p class="text-blue-500 font-bold text-xl">
+            TACKLING, INTERCEPTION & RECOVERY
+          </p>
+          <p class="text-blue-500 font-bold text-xl">DEFENSIVE DUELS</p>
+          <p class="text-blue-500 font-bold text-xl">POSITIONING & SHAPE</p>
+          <p class="text-blue-500 font-bold text-xl">
+            SHOT & CHANCE PREVENTION
+          </p>
+          <p class="text-blue-500 font-bold text-xl">SET-PIECES</p>
+        </div>
+      </div>
+      <div class="p-8 rounded-lg flex flex-col md:flex-row items-center gap-8">
+        <div
+          class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] p-1 rounded-3xl w-full md:w-2/3 shadow-lg"
+        >
+          <div class="bg-white p-6 rounded-3xl">
+            <h2 class="text-xl font-bold text-accent">
+              KEEP YOUR TEAM PERFORMING AT THEIR PEAK
+            </h2>
+            <p class="mt-4 text-black">
+              Track every meter your players run during games or training
+              sessions. Monitor each player's sprinting capabilities. Gain
+              insights into explosive movements and recoveries. Identify when
+              and where your players exert the most effort. Understand the
+              workload your players are handling. Measure physical strain and
+              fatigue levels to optimize performance and prevent injuries.
+            </p>
+            <p class="mt-4 text-accent font-bold">AND MANY MORE!</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 flex flex-col gap-2 text-left">
+          <h3 class="text-2xl font-semibold text-white uppercase">
+            FITNESS METRICS
+          </h3>
+          <div class="border-t border-gray-500 w-full mx-auto mb-4"></div>
+          <p class="text-red-500 font-bold text-xl">DISTANCE COVERED</p>
+          <p class="text-red-500 font-bold text-xl">TOP SPEED</p>
+          <p class="text-red-500 font-bold text-xl">
+            ACCELERATION & DECELERATION
+          </p>
+          <p class="text-red-500 font-bold text-xl">HIGH INTENSITY RUNS</p>
+          <p class="text-red-500 font-bold text-xl">WORK RATE</p>
+          <p class="text-red-500 font-bold text-xl">PLAYER LOAD</p>
+        </div>
+      </div>
+    </section>
+
+    <div
+      class="bg-gray-900 text-white p-8 rounded-lg flex flex-col items-center gap-6"
+    >
+      <div
+        class="bg-gradient-to-r from-gray-900 text-center to-[#2a28a3] text-white px-20 py-4 rounded-lg md:rounded-full uppercase font-bold border border-white"
+      >
+        <h2 class="text-3xl font-bold text-white">
+          GAME HIGHLIGHTS & TACTICAL RECOGNITION
+        </h2>
+        <p class="mt-2 text-gray-200">
+          Relive the key moments of your game with automatically generated
+          highlights, tactical recognition, and in-depth analytics. Whether it's
+          a transition, build-up, or scoring chance, OmniMetrics spots the
+          action for you with precise metrics and performance insights.
+        </p>
+      </div>
+      <div class="md:flex gap-8 items-center">
+        <div
+          class="bg-gradient-to-r from-[#3533cd] to-[#ff4646] p-0.5 shadow-lg"
+        >
+          <img
+            src="/src/assets/img/football-trainer-platform.jpg"
+            alt="Game Highlight"
+            class="shadow-lg w-20% max-h-60"
+          />
+        </div>
+        <div class="relative w-full mt-4 md:mt-0 md:w-1/3 items-end text-left">
+          <p class="text-5xl font-extrabold uppercase text-gray-400">DYNAMIC</p>
+          <p class="text-5xl font-extrabold uppercase text-gray-400 mt-4">
+            EFFORTLESS
+          </p>
+          <p class="text-5xl font-extrabold uppercase text-gray-400 mt-4">
+            ACTIONABLE
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
