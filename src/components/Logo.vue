f8ab943f92a84d7602f12c1d079ee7a62b3cb071
Samuel Fernandes <diogosamuelgfernandes@gmail.com>
2025-02-15 12:42:33 +0000

    feat: more responsive webpage

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -21,7 +21,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-/* No styles needed as Tailwind CSS is used */
-</style>
--- src/views/UniverseView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>This is a universe page</h1>
-  </div>
-</template>
