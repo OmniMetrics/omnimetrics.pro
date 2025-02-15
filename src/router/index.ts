84ec08b74449bef9ec75b8897937cdb97e639fc9
Samuel Fernandes <45127751+samyuh@users.noreply.github.com>
2025-02-15 15:55:46 +0000

    fix: remove hash from url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import FeaturesView from '@/views/FeaturesView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),  // Switch to history mode
   routes: [
     {
       path: '/',
